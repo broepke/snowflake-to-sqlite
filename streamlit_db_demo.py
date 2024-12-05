88323e69f21aa3db596031d9768c115bec333786
Brian Roepke <broepke@gmail.com>
2024-12-05 14:55:26 -0600

    Added a little more text and info to the main page

--- streamlit_db_demo.py
+++ streamlit_db_demo.py
@@ -2,16 +2,21 @@ import streamlit as st
 from utilities import snowflake_connection_helper
 from utilities import run_snowflake_query
 
+st.title("SQLite & Snowflake Intechange Demo")
+st.write("This is a small demo application that can take a single query and pass it to either a remote Snowflake warehouse or a local DB cache of tables in SQLlite.")
+
 query = """select * from picks where year = 2024 and DEATH_DATE is not null"""
 
+st.code(query)
+
 # Query and display the data you inserted
-st.header("SQL Lite")
+st.subheader("SQL Lite")
 sqlite_conn = st.connection("deadpool_db", type="sql")
 sqlite_df = sqlite_conn.query(query)
 st.dataframe(sqlite_df)
 
 
-st.header("Snowflake")
+st.subheader("Snowflake")
 snow_conn = snowflake_connection_helper()
 snow_df = run_snowflake_query(snow_conn, query)
 st.dataframe(snow_df)
